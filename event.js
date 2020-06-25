d385dd4f9a41c8a08dea90e90329c110356e3e7c
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-06-25 23:10:36 +0430

    complate discountcode and calcpayable

--- event.js
+++ event.js
@@ -12,6 +12,7 @@ $(".plusmark").on("click",function(){
    number.text(numberInt+1)
    totalPrice.text(+unitprice +totalpriceNumber)
    calAllTotalPricePlus(unitprice)
+   calcpayable()
 
 })
 $(".minusmark").on("click",function(){
@@ -25,6 +26,7 @@ $(".minusmark").on("click",function(){
     number.text(numberInt-1)
     totalPrice.text(totalpriceNumber-(+unitprice))
     calAllTotalPriceMinus(unitprice)
+    calcpayable()
     }
 
  })
@@ -35,4 +37,68 @@ $(".minusmark").on("click",function(){
  function calAllTotalPriceMinus(value){
     $(".sumTotalPrices-number").text(parseInt($(".sumTotalPrices-number").text()) - (+value))
     $(".servisCharge-number").text((parseInt($(".sumTotalPrices-number").text()))*0.09)
+ }
+ let m="empty"
+ let n=true
+ $(".discountCode-button").on("click",function(){
+   if(m=="empty" ){
+   for (key in discountCodes)
+   {
+      
+      
+      
+      if($(".discountCode-input").val()==key)
+      {
+         $(".discount-number").text(discountCodes[key])
+         $(".discountCode-input").addClass("correctCode")
+         $(".discountCode-button").html('<i class="fas fa-trash"></i>')
+         $(".discountCode-button").addClass("correctCodebtn")
+         m="correct"
+         calcpayable()
+      }
+      
+      if(m!="correct")
+      m="wrong"
+   }
+}
+  else if(m=="correct")
+   {
+         $(".discount-number").text("0")
+         $(".discountCode-input").removeClass("correctCode")
+         $(".discountCode-button").html('<i class="fas fa-plus "></i>')
+         $(".discountCode-button").removeClass("correctCodebtn")
+         $(".discountCode-input").val("")
+         m="empty"
+         calcpayable()
+   }
+   
+
+   if(m=="wrong")
+   {
+      $(".discountCode-input").addClass("wrongtCode")
+      m="empty"
+      n=false
+   }
+
+      
+   
+
+})
+$(".discountCode-input").on("keydown",function(){
+   if(!n)
+   {
+   $(".discountCode-input").removeClass("wrongtCode")
+      n=true
+   }
+})
+ const discountCodes={
+    first:-20000,
+    second:-30000,
+    third:-40000 
+ }
+ function calcpayable()
+ {
+$(".payablePrice-number").text((+$(".sumTotalPrices-number").text())+(+$(".servisCharge-number").text())+(+$(".discount-number").text()))
+  
+
  }
